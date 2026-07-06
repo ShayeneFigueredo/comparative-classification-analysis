98809c4856b2a7f0cb08df015be00c6cfc322f80
ShayeneFigueredo <shayenefigueredo@gmail.com>
2026-07-06 18:30:05 -0300

    atualizacoes no codigo car_evaluation.py

--- codigo/shayene/car_evaluation.py
+++ codigo/shayene/car_evaluation.py
@@ -8,4 +8,45 @@ Executar a partir da raiz do repositório:
     python codigo/shayene/car_evaluation.py
 """
 
-# O código será construído passo a passo aqui.
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#dei nome para as colunas pq o arquivo nao tem cabeçalho
+COLUNAS = ['p_compra', 'p_manutencao', 'portas', 'capacidade', 'porta_malas', 'seguranca', 'classe']
+
+df= pd.read_csv('base_dados/car+evaluation/car.data', names=COLUNAS)
+
+#ver quantos carros sao unacc, acc, good e vgood
+# print(df['classe'].value_counts())
+
+#criando grafico de barras da distribuição de classe
+df['classe'].value_counts().plot(kind='bar', color='steelblue', edgecolor='black')
+plt.title("Distribuição da Classe - Car Evaluation")
+plt.xlabel('Classe')
+plt.ylabel('Quantidade')
+# plt.savefig('resultados/shayene/figuras/distribuicao_classe.png', dpi=150)
+
+
+#PROCESSAMENTO DOS DADOS
+from sklearn.preprocessing import LabelEncoder
+
+#criando copia do dataframe pra nao bagunçar o original
+df_cod = df.copy()
+
+le = LabelEncoder()
+
+for coluna in df_cod.columns:
+    df_cod[coluna] = le.fit_transform(df_cod[coluna])
+
+# print(df_cod.head())
+# print()
+# print(df_cod['classe'].value_counts())
+
+
+#separer features (x) da classe (y)
+X = df_cod.drop('classe', axis=1)
+y = df_cod['classe']
+
+print('Features:', X.shape)
+print('Classe:', y.shape)
